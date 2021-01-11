a06c2eede9010386883a0c634588bc4a004ac955
sangbala <aaeuio@gmail.com>
2021-01-11 15:20:15 -0800

    SummativeSums

---
+++ src/main/java/com/mycompany/summativesums/SummativeSums.java
@@ -0,0 +1,39 @@
+ 
+package com.mycompany.summativesums;
+
+ 
+public class SummativeSums {
+    
+    public static void main(String[] args){
+        
+        int[] arr1={ 1, 90, -33, -55, 67, -16, 28, -55, 15 };
+        int[] arr2={ 999, -60, -77, 14, 160, 301 };
+        int[] arr3={ 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 
+140, 150, 160, 170, 180, 190, 200, -99 } 
+;
+        int ans1=calSum(arr1);
+        int ans2=calSum(arr2);
+        int ans3=calSum(arr3);
+        
+        System.out.println("#1 Array Sum: "+ans1);
+        System.out.println("#2 Array Sum: "+ans2);
+        System.out.println("#3 Array Sum: "+ans3);
+    }
+    
+
+    private static int calSum(int[] arr){
+        
+        if(arr.length==0){
+            return 0;
+        }
+        
+        int ans=0;
+        for(int i=0;i<arr.length;i++){
+            
+            ans+=arr[i];
+            
+        }
+        return ans;
+    }
+    
+}
